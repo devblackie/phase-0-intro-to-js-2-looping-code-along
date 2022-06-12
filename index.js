1a28f2e32904346b0bb58c6307555230550e14a8
devblackie <newtieblackie@gmail.com>
2022-06-12 18:04:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // Code your solutions in this file
-let names=[]
-function writeCards(names, event) {
-  const messages = [];
-  for (let i = 0; i < names.length; i++) {
-    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-    messages.push(message);
-  }
-  return messages;
+for(let age=30; age<40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
 }
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number);
-        number = number - 1;
-    }
-} 
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+for (let i = 0; i < gifts.length; i++) {
+  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  debugger;
+}
+
+return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards( arrayNames, event ) {
+  let myContainer = [];
+  for ( let i = 0; i < arrayNames.length; i++ ) {
+    myContainer.push( `Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!` )
+  }
+  return myContainer;
+}
+function countDown(num){
+  while(num>=0){
+      console.log(num);
+      num--;
+  }
+}countDown(10)
